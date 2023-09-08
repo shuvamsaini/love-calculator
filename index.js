9332a0e23a1fdba063958a4573994636add0ebb7
shuvamsaini <46352033+shuvamsaini@users.noreply.github.com>
2023-09-08 13:54:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var yourName = prompt("What is Your Name");
+var crushName = prompt("What is Your Crush Name");
+
+var loveCalculator = Math.random();
+loveCalculator = Math.floor(loveCalculator * 100) + 1;
+
+alert("Chances of Relationship between " + yourName + " and " + crushName + " is " + loveCalculator + "%");
